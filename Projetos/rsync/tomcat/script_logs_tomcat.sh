66c05273d2d72781fa8e64b6309abf425242cd76
Matheus-SCosta <silvadacosta2018@gmail.com>
2021-02-04 16:11:25 -0300

    Exemplos

---
+++ Projetos/rsync/tomcat/script_logs_tomcat.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source script_logs_tomcat.conf
+
+#verificando se a chave existe
+ls ${chave} &>/dev/null || echo -e "Chave ${chave} não existente \nTransferencia de logs não realizada \nVerificar em /home/centos/script_logs_tomcat.conf"
+ls ${chave} &>/dev/null || exit 1
+
+#liberando a chain INPUT do iptables para fazer o ssh
+iptables -P INPUT ACCEPT
+
+#testando se o usuário é existente no servidor  de logs ou se o ip está incorreto
+existencia_usuario=true
+ssh -i ${chave} ${usuario}@${ip_destino} pwd &>/dev/null || existencia_usuario=false
+if [ ${existencia_usuario} == "false" ];then
+        echo -e "Usuário ${usuario} não existente em ${ip_destino} ou ${ip_destino} não pertence ao servidor de logs \nTransferencia de logs não realizada \nVerificar em /home/centos/script_logs_tomcat.conf"
+        exit 1
+fi
+
+#Verificando existência de diretório de destino no servidor de logs
+ssh -i chave.pem ${usuario}@${ip_destino} test -d ${diretorio_destino} && existencia_diretorio_destino=true || existencia_diretorio_destino=false
+if [ $existencia_diretorio_destino == "false" ];then
+        echo -e "Diretório ${diretorio_destino} não existe em ${ip_destino} \nTransferencia de logs não realizada \nVerificar em /home/centos/script_logs_tomcat.conf"
+        exit 1
+fi
+
+#verificar se o Path dos logs nginx existe
+if [ ! -d ${PATH_logs} ]; then
+        echo -e "Diretório ${PATH_logs} não existe em serviço local \nTransferencia de logs não realizada \nVerificar em /home/centos/script_logs_tomcat.conf"
+        exit 1
+fi
+
+#Comando
+rsync -ahvpu --progress -e "ssh -i ${chave}" ${PATH_logs} ${usuario}@${ip_destino}:${diretorio_destino} 2>/dev/null
+
+
+#Restaurando a regra padrão ipbtales para INPUT como DROP
+iptables -P INPUT DROP
+
